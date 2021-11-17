87314edf2f251de7ab0d6a59f37c72de4134e76b
Miguel López <miguelopezv@gmail.com>
2021-11-16 22:42:09 -0500

    validate query params

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from typing import Optional
 from pydantic import BaseModel
 from fastapi import FastAPI
 from fastapi import Body
+from fastapi import Query
 
 app = FastAPI()
 
@@ -20,5 +21,15 @@ def home():
 
 
 @app.post('/person/new')
+# request body
 def create_person(person: Person = Body(...)):
     return person
+
+
+# Query params validation
+@app.get('/person/details')
+def show_person(
+    name: Optional[str] = Query(None, min_length=1, max_length=50),
+    age: int = Query(...)
+):
+    return {name: age}
